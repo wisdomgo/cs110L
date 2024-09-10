8d196ac868f236608ed7133af65e3e168cf8d3c5
wisdomgo <1246045735@qq.com>
2024-09-11 00:59:55 +0800

    finish part3

--- week1/part-3-hangman/src/main.rs
+++ week1/part-3-hangman/src/main.rs
@@ -14,6 +14,7 @@
 // more in depth in the coming lectures.
 extern crate rand;
 use rand::Rng;
+use std::collections::HashSet;
 use std::fs;
 use std::io;
 use std::io::Write;
@@ -26,7 +27,21 @@ fn pick_a_random_word() -> String {
     let words: Vec<&str> = file_string.split('\n').collect();
     String::from(words[rand::thread_rng().gen_range(0, words.len())].trim())
 }
+fn print_word_so_far(word: &Vec<char>) {
+    print!("The word so far is ");
+    for i in word.iter() {
+        print!("{}",i);
+    }
+    println!("");
+}
 
+fn print_guessed_word(word: &HashSet<char>) {
+    print!("You have guessed the following letters");
+    for i in word.iter(){
+        print!("{}",i);
+    }
+    println!("");
+}
 fn main() {
     let secret_word = pick_a_random_word();
     // Note: given what you know about Rust so far, it's easier to pull characters out of a
@@ -34,7 +49,50 @@ fn main() {
     // secret_word by doing secret_word_chars[i].
     let secret_word_chars: Vec<char> = secret_word.chars().collect();
     // Uncomment for debugging:
-    // println!("random word: {}", secret_word);
-
     // Your code here! :)
+    let mut word_so_far = Vec::new();
+    let mut not_guessed_words = HashSet::new();
+    let mut guessed_words = HashSet::new();
+    let mut idx = NUM_INCORRECT_GUESSES;
+    let len = secret_word.len();
+    for i in 0..len {
+        word_so_far.push('-');
+        not_guessed_words.insert(secret_word_chars[i]);
+    }
+    println!("Welcome to CS110L Hangman!");
+    loop {
+        print_word_so_far(&word_so_far);
+        print_guessed_word(&guessed_words);
+        println!("You have {} guesses left",idx);
+        print!("Please guess a letter:");
+        io::stdout()
+            .flush()
+            .expect("Error flushing stdout");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading line");
+        let guess_char: Vec<char> = guess.chars().collect();
+        if not_guessed_words.contains(&guess_char[0]){
+            not_guessed_words.remove(&guess_char[0]);
+            guessed_words.insert(guess_char[0]);
+            for i in 0..len {
+                if guess_char[0] == secret_word_chars[i] {
+                    word_so_far[i] = guess_char[0];
+                }
+            }
+        } else {
+            idx -= 1;
+            println!("Sorry, that letter is not in the word");
+        }
+        println!("");
+        if idx == 0 {
+            println!("Sorry, you ran out of guesses!");
+            break;
+        }
+        if not_guessed_words.is_empty() {
+            println!("Congratulations you guessed the secret word: {}!",secret_word);
+            break;
+        }
+    }
 }
